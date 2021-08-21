0689c93b5957c3e315fb3d2131a3cd45019c9782
Benicio Lopez <benicio.lopez@gmail.com>
2021-08-21 10:39:29 -0500

    hardcoded temp wind and humidity in Austin displays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,37 @@
 var apiKey = "456382b69ba78bc0d18ae825d9b6baff";
 var citySearchInput = document.querySelector("#citySearchInput");
 var citySearchBtn = document.querySelector("#citySearchBtn");
-var getCurrentWeather = function () {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=" + apiKey;
+var presentResultsEl = document.querySelector("#presentResults");
+// var getCurrentWeather = function () {
+//     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=" + apiKey;
+//     fetch(apiUrl)
+//     .then(response => response.json())
+//     .then(data => console.log(data));
+// }
+
+var getWeatherCity = function () {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Austin&appid=" + apiKey;
     fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => console.log(data));
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            var temp = data.main.temp;
+            console.log(temp);
+            var wind = data.wind.speed;
+            var humidity = data.main.humidity
+
+            var tempEl = document.createElement("div")
+            tempEl.textContent = "Temp: " + temp;
+            presentResultsEl.appendChild(tempEl);
+
+            var windEl = document.createElement("div");
+            windEl.textContent = "Wind: " + wind;
+            presentResultsEl.appendChild(windEl);
+
+            var humidityEl = document.createElement("div");
+            humidityEl.textContent = "Humidity: " + humidity;
+            presentResultsEl.appendChild(humidityEl);
+        });
 }
-      
-citySearchBtn.addEventListener("click", getCurrentWeather)
+
+citySearchBtn.addEventListener("click", getWeatherCity)
