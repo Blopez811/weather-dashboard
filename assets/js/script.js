841c5e51e3e8766fde372ba3a9643d87bf571dcf
Benicio Lopez <benicio.lopez@gmail.com>
2021-08-21 09:51:14 -0500

    hardcoded chicago in fetch request in function and it is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 var apiKey = "456382b69ba78bc0d18ae825d9b6baff";
 var citySearchInput = document.querySelector("#citySearchInput");
 var citySearchBtn = document.querySelector("#citySearchBtn");
-var getCurrentWeather = function() {
-    console.log("getCurrentWeather fired!")
-};
-
+var getCurrentWeather = function () {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=" + apiKey;
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+      
 citySearchBtn.addEventListener("click", getCurrentWeather)
