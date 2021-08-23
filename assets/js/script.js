6d2fc0d39eac82fbe44d4cb385521de6b26ff23f
Benicio Lopez <benicio.lopez@gmail.com>
2021-08-22 22:22:05 -0500

    added working buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,35 @@ var apiKey = "456382b69ba78bc0d18ae825d9b6baff";
 var citySearchInput = document.querySelector("#citySearchInput");
 var citySearchBtn = document.querySelector("#citySearchBtn");
 var presentResultsEl = document.querySelector("#presentResults");
+let futureResultsEl = document.querySelector("#futureResults");
+let searchHistoryEl = document.querySelector("#searchHistory")
+let searchHistory = localStorage.getItem("City");
+if (!searchHistory) {
+    searchHistory = [];
+} else {
+    searchHistory = JSON.parse(searchHistory);
+}
+
+let buttonFunction = function () {
+    userInput = this.textContent;
+    citySearchInput.value = userInput;
+    getWeatherCity();
+    getFiveDays();
+    saveSearchHistory();
+    console.log(userInput)
+}
+
+let displayHistory = function () {
+    searchHistoryEl.textContent = "";
+    for (i = 0; i < searchHistory.length; i++) {
+        let searchHistoryBtn = document.createElement("button");
+        searchHistoryBtn.addEventListener("click", buttonFunction)
+        searchHistoryBtn.textContent = searchHistory[i];
+        searchHistoryEl.appendChild(searchHistoryBtn);
+    }
+}
+
+displayHistory();
 var getCurrentWeather = function (coord) {
     let coordLat = coord.lat;
     let coordLon = coord.lon;
@@ -53,19 +82,28 @@ var getWeatherCity = function () {
 }
 
 var getFiveDays = function () {
+    futureResultsEl.textContent = "";
     let apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearchInput.value + "&appid=" + apiKey;
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
             console.log(data, "this is what you need");
-            let dayOneDate = data.list[4].dt_txt
-            let dayOneTemp =  data.list[4].main.temp
-            let dayOneWind =  data.list[4].wind.speed
-            let dayOneHumidity = data.list[4].main.humidity 
-            let dayOneText = dayOneDate + " " + "Temp: " + dayOneTemp + " " + "Wind: " + dayOneWind + " " + "Humidity: " + dayOneHumidity;
-            console.log(dayOneText) 
-            
-            
+
+            //TODO write a for loop
+            for (i = 3; i < data.list.length; i += 8) {
+
+                let dayOneDate = data.list[i].dt_txt
+                let dayOneTemp = data.list[i].main.temp
+                let dayOneWind = data.list[i].wind.speed
+                let dayOneHumidity = data.list[i].main.humidity
+                let dayOneText = dayOneDate + " " + "Temp: " + dayOneTemp + " " + "Wind: " + dayOneWind + " " + "Humidity: " + dayOneHumidity;
+                let dayOneEl = document.createElement("div");
+                dayOneEl.textContent = dayOneText;
+                futureResultsEl.appendChild(dayOneEl);
+            }
+
+
+
             // console.log(data.list[4].main.temp);
             // console.log(data.list[12].main.temp);
             // console.log(data.list[20].main.temp)
@@ -73,6 +111,14 @@ var getFiveDays = function () {
             // console.log(data.list[36].main.temp)
         })
 }
+
+var saveSearchHistory = function () {
+    searchHistory.push(citySearchInput.value)
+    localStorage.setItem("City", JSON.stringify(searchHistory));
+    console.log(searchHistory);
+    displayHistory();
+}
 citySearchBtn.addEventListener("click", getWeatherCity)
 // citySearchBtn.addEventListener("click", getCurrentWeather)
 citySearchBtn.addEventListener("click", getFiveDays);
+citySearchBtn.addEventListener("click", saveSearchHistory);
